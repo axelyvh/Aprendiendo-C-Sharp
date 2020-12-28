1f432b701a488167d5983ef3583fa3b42ad4b78f
Axel Yoliston Vasquez Herrera <axelvasquezherrera@outlook.com>
2020-12-28 00:56:56 -0500

    Inyeccion de dependencias

---
+++ RESTFul API .NET Core 3.1/SocialMedia/SocialMedia.Core/Interfaces/IPostRepository.cs
@@ -0,0 +1,11 @@
+﻿using SocialMedia.Core.Entitties;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Core.Interfaces
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> GetPosts();
+    }
+}
---
+++ RESTFul API .NET Core 3.1/SocialMedia/SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,30 @@
+﻿using SocialMedia.Core.Entitties;
+using SocialMedia.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+
+        public async Task<IEnumerable<Post>> GetPosts() {
+
+            var posts = Enumerable.Range(1, 10).Select(x => new Post { 
+                PostId = x,
+                Description = $"Descripcion {x}",
+                Date = DateTime.Now,
+                Image = $"htpps://miapis.com/{x}",
+                UserId = x * 2
+            });
+
+            await Task.Delay(1000);
+
+            return posts;
+
+        }
+
+    }
+}
